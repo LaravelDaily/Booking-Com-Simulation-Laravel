7f76c361fa7c4f19eff0b5ef03af799a8bea2a73
PovilasKorop <povilask007@gmail.com>
2023-03-26 14:15:17 +0200

    Feature test for placing the booking

--- tests/Feature/BookingsTest.php
+++ tests/Feature/BookingsTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Apartment;
+use App\Models\City;
+use App\Models\Property;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -10,6 +13,23 @@ use Tests\TestCase;
 class BookingsTest extends TestCase
 {
     use RefreshDatabase;
+
+    private function create_apartment(): Apartment
+    {
+        $owner = User::factory()->create(['role_id' => Role::ROLE_OWNER]);
+        $cityId = City::value('id');
+        $property = Property::factory()->create([
+            'owner_id' => $owner->id,
+            'city_id' => $cityId,
+        ]);
+
+        return Apartment::create([
+            'name' => 'Apartment',
+            'property_id' => $property->id,
+            'capacity_adults' => 3,
+            'capacity_children' => 2,
+        ]);
+    }
 
     public function test_user_has_access_to_bookings_feature()
     {
@@ -25,5 +45,30 @@ class BookingsTest extends TestCase
         $response = $this->actingAs($owner)->getJson('/api/user/bookings');
 
         $response->assertStatus(403);
+    }
+
+    public function test_user_can_book_apartment_successfully_but_not_twice()
+    {
+        $user = User::factory()->create(['role_id' => Role::ROLE_USER]);
+        $apartment = $this->create_apartment();
+
+        $bookingParameters = [
+            'apartment_id' => $apartment->id,
+            'start_date' => now()->addDay(),
+            'end_date' => now()->addDays(2),
+            'guests_adults' => 2,
+            'guests_children' => 1,
+        ];
+        $response = $this->actingAs($user)->postJson('/api/user/bookings', $bookingParameters);
+        $response->assertStatus(201);
+
+        $response = $this->actingAs($user)->postJson('/api/user/bookings', $bookingParameters);
+        $response->assertStatus(422);
+
+        $bookingParameters['start_date'] = now()->addDays(3);
+        $bookingParameters['end_date'] = now()->addDays(4);
+        $bookingParameters['guests_adults'] = 5;
+        $response = $this->actingAs($user)->postJson('/api/user/bookings', $bookingParameters);
+        $response->assertStatus(422);
     }
 }
