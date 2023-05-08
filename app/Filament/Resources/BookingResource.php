ecb87a9482939d460cbabb51b14bb11218c54bed
Nerijus <krekas@gmail.com>
2023-05-08 16:19:11 +0300

    Booking

---
+++ app/Filament/Resources/BookingResource.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BookingResource\Pages;
+use App\Filament\Resources\BookingResource\RelationManagers;
+use App\Models\Booking;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BookingResource extends Resource
+{
+    protected static ?string $model = Booking::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('apartment_id')
+                    ->required()
+                    ->searchable()
+                    ->relationship('apartment', 'name'),
+                Forms\Components\Select::make('user_id')
+                    ->required()
+                    ->searchable()
+                    ->relationship('user', 'name'),
+                Forms\Components\DatePicker::make('start_date')
+                    ->required(),
+                Forms\Components\DatePicker::make('end_date')
+                    ->required(),
+                Forms\Components\TextInput::make('guests_adults')
+                    ->integer()
+                    ->required()
+                    ->minValue(0),
+                Forms\Components\TextInput::make('guests_children')
+                    ->integer()
+                    ->required()
+                    ->minValue(0),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('apartment.name'),
+                Tables\Columns\TextColumn::make('start_date')
+                    ->date(),
+                Tables\Columns\TextColumn::make('end_date')
+                    ->date(),
+                Tables\Columns\TextColumn::make('total_price')
+                    ->money(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()
+                    ->label('Cancel'),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make()
+                    ->label('Cancel'),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBookings::route('/'),
+            'create' => Pages\CreateBooking::route('/create'),
+            'edit' => Pages\EditBooking::route('/{record}/edit'),
+        ];
+    }
+}
